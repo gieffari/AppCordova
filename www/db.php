43410a97f54298c4a16578f85ce5e1a1f28f9fa7
gieffari <48472651+gieffari@users.noreply.github.com>
2019-05-14 21:54:03 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8941235_admin","cacastie","id8941235_canteen") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id9044538_gieffari01","abdillah","id9044538_cacastiepkl") or die ("could not connect database");
 ?>
