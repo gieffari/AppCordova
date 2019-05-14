dc36d3425eaf7838f08b9abdf7c08937293077d4
gieffari <48472651+gieffari@users.noreply.github.com>
2019-05-14 21:54:45 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -1,12 +1,13 @@
 <?php
  include "db.php";
- if(isset($_GET['id_makanan']))
+ if(isset($_GET['id_minuman']))
  {
- $id_makanan=$_GET['id_makanan'];
- $q=mysqli_query($con,"delete from `makanan` where `id_makanan`='$id_makanan'");
+ $id_minuman=$_GET['id_minuman'];
+ $q=mysqli_query($con,"delete from `minuman` where `id_minuman`='$id_minuman'");
  if($q)
  echo "success";
  else
  echo "error";
  }
  ?>
+
